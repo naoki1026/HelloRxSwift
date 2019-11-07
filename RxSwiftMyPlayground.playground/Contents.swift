fdfdfd132835f579a6912ced4654254719009939
arakawan <arakawan@pivot.jp>
2019-11-07 23:12:41 +0900

    レッスン１５まで

--- RxSwiftMyPlayground.playground/Contents.swift
+++ RxSwiftMyPlayground.playground/Contents.swift
@@ -2,9 +2,24 @@ import UIKit
 import RxSwift
 
 let disposeBag = DisposeBag()
-let subject = BehaviorSubject(value: "Initial Value")
-subject.subscribe { event in
-    print(event)
+let subject = ReplaySubject<String>.create(bufferSize: 2)
+
+//bufferSizeで指定したイベントのうち、最後から２つのイベントが実行される
+subject.onNext("Issue 1")
+subject.onNext("Issue 2")
+subject.onNext("Issue 3")
+
+subject.subscribe {
+    print($0)
 }
 
-subject.onNext("Issue 1")
+subject.onNext("Issue 4")
+subject.onNext("Issue 5")
+subject.onNext("Issue 6")
+
+print("[Subscription 2]")
+
+//2,3,4,5,6,Subscription2,5,6となる
+subject.subscribe {
+    print($0)
+}
