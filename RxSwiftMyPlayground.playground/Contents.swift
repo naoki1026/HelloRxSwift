f9126cc49f6d2d7d6d6c53c25e781caadb516182
arakawan <arakawan@pivot.jp>
2019-11-07 21:47:43 +0900

    レッスン10まで

--- RxSwiftMyPlayground.playground/Contents.swift
+++ RxSwiftMyPlayground.playground/Contents.swift
@@ -2,9 +2,37 @@ import UIKit
 import RxSwift
 
 let observable = Observable.just(1)
-
 let observable2 = Observable.of(1,2,3)
-
-let observavle3 = Observabke.of([1,2,3])
-
+let observavle3 = Observable.of([1,2,3])
 let observable4 = Observable.from([1,2,3,4,5])
+
+
+/////////////////////////////////////////
+
+observavle3.subscribe { event in
+    print(event)
+}
+
+observavle3.subscribe { event in
+    if let element = event.element {
+        print(element)
+    }
+}
+
+/////////////////////////////////////////
+
+observable4.subscribe {event in
+    print(event)
+}
+
+observable4.subscribe { event in
+    if let element = event.element {
+        print(element)
+    }
+}
+
+observable4.subscribe(onNext: {element in
+    print(element)
+})
+
+/////////////////////////////////////////
