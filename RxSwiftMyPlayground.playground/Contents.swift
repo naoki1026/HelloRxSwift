d3d69f0428bd03b3f5b2671cab009f2ac1a935e6
arakawan <arakawan@pivot.jp>
2019-11-07 23:04:03 +0900

    レッスン15まで

--- RxSwiftMyPlayground.playground/Contents.swift
+++ RxSwiftMyPlayground.playground/Contents.swift
@@ -2,23 +2,9 @@ import UIKit
 import RxSwift
 
 let disposeBag = DisposeBag()
-let subject = PublishSubject<String>()
-
-
-//subscribeが行われた後でないとeventが発生しない
-subject.onNext("Issue1")
-
+let subject = BehaviorSubject(value: "Initial Value")
 subject.subscribe { event in
     print(event)
 }
 
-subject.onNext("Issue2")
-subject.onNext("Issue3")
-
-//subject.dispose()
-
-subject.onCompleted()
-
-//disposeの後はeventが発生しない
-//onCompletedの後もeventは発生しない
-subject.onNext("Issue5")
+subject.onNext("Issue 1")
