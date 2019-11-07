abc2def15099eab487e4ad15f7d69e9b5a38f35a
arakawan <arakawan@pivot.jp>
2019-11-07 22:26:43 +0900

    レッスン１１まで

--- RxSwiftMyPlayground.playground/Contents.swift
+++ RxSwiftMyPlayground.playground/Contents.swift
@@ -1,47 +1,19 @@
 import UIKit
 import RxSwift
 
-/////////////////////////////////////////
+let disposeBag = DisposeBag()
 
-let observable = Observable.just(1)
-let observable2 = Observable.of(1,2,3)
-let observavle3 = Observable.of([1,2,3])
-let observable4 = Observable.from([1,2,3,4,5])
-
-
-/////////////////////////////////////////
-
-observavle3.subscribe { event in
-    print(event)
-}
-
-observavle3.subscribe { event in
-    if let element = event.element {
-        print(element)
-    }
-}
-
-/////////////////////////////////////////
-
-observable4.subscribe {event in
-    print(event)
-}
-
-observable4.subscribe { event in
-    if let element = event.element {
-        print(element)
-    }
-}
-
-observable4.subscribe(onNext: {element in
-    print(element)
-})
-
-let subscription4 = observable4.subscribe(onNext: {element in
-    print(element)
-})
-
-subscription4.dispose()
+Observable.of("A", "B", "C")
+    .subscribe {
+        print($0)
+    }.disposed(by: disposeBag)
 
 
-/////////////////////////////////////////
+//onCompletedで処理が終わっているため？は呼ばれない
+Observable<String>.create { observer in
+    observer.onNext("A")
+    observer.onCompleted()
+    observer.onNext("?")
+    return Disposables.create()
+    }.subscribe(onNext: {print($0)}, onError: {print($0)}, onCompleted: {print("Completed")}, onDisposed: {print("Disposed")})
+    .disposed(by: disposeBag)
